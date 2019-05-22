c515d5926d4810b5fa819c4b26c6f1612d5a0d87
shawroc <coldplay1204@hotmail.com>
2019-05-22 11:53:51 +0800

    add a new blog

--- webpackDemo/src/index.js
+++ webpackDemo/src/index.js
@@ -1,16 +1,9 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
   const element = document.createElement('div');
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['hello', 'webpack'], ' ');
   return element;
-  }).catch(error => 'An error occurred while loading the component');
-};
+}
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+document.body.appendChild(component());
